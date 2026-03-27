b6f0e4e2e3107f9393fda9372d456f9129c21c2e
Muhammad Aska Alghifari <askaalghifari487@gmail.com>
2026-03-27 19:19:45 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function Tampilpesans() {
+    alert("Juara 2 Tari Kreasi Se-Sumatra Selatan 2022");
+}
+function Tampilpesand() {
+    alert("Juara 2 LTBB Pramuka se-sumatra selatan 2023");
+}
+function Tampilpesant() {
+    alert("Putri Literasi 2024");
+}
